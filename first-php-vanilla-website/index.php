2a585fa49f72e314bdaf0228fa773e1282a71a1c
mikolaszko <mikosodz.lote@gmail.com>
2023-05-31 20:51:02 +0200

    I shouldve probably skipped the first few episodes honestly

---
+++ first-php-vanilla-website/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>My Website</title>
+    <meta name="description" content="My Website">
+    <meta name="author" content="mikolaszko">
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Hello World!";
+        ?>
+    </h1>
+</body>
+</html>
+
