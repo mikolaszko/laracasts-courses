b6451c43e879bbb327e283729362ea50295fa147
mikolaszko <mikosodz.lote@gmail.com>
2023-05-31 23:49:18 +0200

    Work with for each loops inside the php tags>

--- first-php-vanilla-website/index.php
+++ first-php-vanilla-website/index.php
@@ -5,14 +5,50 @@
     <title>My Website</title>
     <meta name="description" content="My Website">
     <meta name="author" content="mikolaszko">
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 <body>
+    <?php
+        $name = "Dark Matter";
+        $read = true;
+        $string;
+        
+
+        if ($read) {
+            $string = "You have read \"$name\".";
+        } else {
+            $string = "You haven't read \"$name\".";
+        }
+        $books = [
+            "Do Androids Dream of Electric Sheep?",
+            "Neuromancer",
+            "Brave New World"
+        ];
+    ?>
     <h1>
-        <?php 
-            $greeting = "Hi";
-            echo "$greeting World!";
-        ?>
+        <?= $string ?>
     </h1>
+    <h4>Recommended Books</h4>
+    <!-- long way -->
+    <!--<ul>
+        <?php foreach ($books as $book) {
+            echo "<li>$book</li>";
+        } ?>
+    </ul>-->
+    <!-- shorthand -->
+    <ul>
+        <?php foreach ($books as $book) : ?>
+            <li><?= $book ?></li>
+        <?php endforeach; ?>
+    </ul>
 </body>
 </html>
 
