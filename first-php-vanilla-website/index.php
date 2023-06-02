51e73e3c704a976d8b237b20f9b7db3a22fc4dd7
mikolaszko <mikosodz.lote@gmail.com>
2023-06-02 14:13:41 +0200

    Refactor of project structure

--- first-php-vanilla-website/index.php
+++ first-php-vanilla-website/index.php
@@ -1,86 +1,56 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>my website</title>
-    <meta name="description" content="my website">
-    <meta name="author" content="mikolaszko">
-    <style>
-        body {
-            display: grid;
-            place-items: center;
-            height: 100vh;
-            margin: 0;
-            font-family: sans-serif;
-        }
-    </style>
-</head>
-<body>
-    <?php
-        $name = "dark matter";
-        $read = true;
-        $string;
-        
-
-        if ($read) {
-            $string = "you have read \"$name\".";
-        } else {
-            $string = "you haven't read \"$name\".";
-        }
+<?php
+$name = "dark matter";
+$read = true;
+$string;
 
 
-        $books = [
-                    [
-                'name' => 'do androids dream of electric sheep?',
-                'author' => "philip k. dick",
-                'purchase' => 'https://google.com',
-                'release_year' => 1968
-            ],
+if ($read) {
+    $string = "you have read \"$name\".";
+} else {
+    $string = "you haven't read \"$name\".";
+}
+
+
+$books = [
             [
-                'name' => 'brave new world',
-                'author' => "Aldous huxley",
-                'purchase' => 'https://google.com',
-                'release_year' => 1931
-            ],
-            [
-                'name' => 'The Martian',
-                'author' => "Andy Weir",
-                'purchase' => 'https://google.com',
-                'release_year' => 2011 
-            ],
-            [
-                'name' => 'Project Hail Mary',
-                'author' => "Andy Weir",
-                'purchase' => 'https://google.com',
-                'release_year' => 2021 
-            ],
-        ];
-        //lambda function -> fully valid but redundant
-        $filter = function (array $items, $fn) {
-            $filteredItems = [];
-            foreach ($items as $item) {
-                if ($fn($item)) {
-                    $filteredItems[] = $item;
-                } 
-            }
-            return $filteredItems;
-        };
+        'name' => 'do androids dream of electric sheep?',
+        'author' => "philip k. dick",
+        'purchase' => 'https://google.com',
+        'release_year' => 1968
+    ],
+    [
+        'name' => 'brave new world',
+        'author' => "Aldous huxley",
+        'purchase' => 'https://google.com',
+        'release_year' => 1931
+    ],
+    [
+        'name' => 'The Martian',
+        'author' => "Andy Weir",
+        'purchase' => 'https://google.com',
+        'release_year' => 2011 
+    ],
+    [
+        'name' => 'Project Hail Mary',
+        'author' => "Andy Weir",
+        'purchase' => 'https://google.com',
+        'release_year' => 2021 
+    ],
+];
+//lambda function -> fully valid but redundant
+$filter = function (array $items, $fn) {
+    $filteredItems = [];
+    foreach ($items as $item) {
+        if ($fn($item)) {
+            $filteredItems[] = $item;
+        } 
+    }
+    return $filteredItems;
+};
 
-        //php build in array function
-        $filteredBooks = array_filter($books, function ($book) {
-            return $book['release_year'] >= 1950;
-        });
-    ?>
+//php build in array function
+$filteredBooks = array_filter($books, function ($book) {
+    return $book['release_year'] >= 1950;
+});
 
-
-    <h4>recommended books</h4>
-    <ul>
-        <?php foreach ($filteredBooks as $book) : ?>
-            <a href="<?= $book['purchase'] ?>">
-                <li><?= $book['name'] ?> (<?= $book['release_year'] ?>)</li>
-            </a>
-        <?php endforeach; ?>
-    </ul>
-</body>
-</html>
-
+require "index.view.php";
