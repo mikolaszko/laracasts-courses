84aea8ebc30377a69591deadbd8917a269a00d4f
mikolaszko <mikosodz.lote@gmail.com>
2023-06-02 15:03:22 +0200

    Partials

---
+++ first-php-vanilla-website/views/partials/head.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html class="h-full bg-gray-100" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>my website</title>
+        <meta name="description" content="my website">
+        <meta name="author" content="mikolaszko">
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+<body class="h-full">
