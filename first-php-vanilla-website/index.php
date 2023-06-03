79b24966bb75c4feed826480f137d9b9d1084d13
mikolaszko <mikosodz.lote@gmail.com>
2023-06-03 23:53:48 +0200

    DB setup with manual dsn and sql query

--- first-php-vanilla-website/index.php
+++ first-php-vanilla-website/index.php
@@ -2,3 +2,16 @@
 
 require 'utils/functions.php';
 require 'router.php';
+
+// connect to out MySQL database.
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4;";
+$pdo = new PDO($dsn, 'root', 'root');
+
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li> {$post['title']} </li>"; 
+}
