35c73e18d92fd279f1e2c48076f65e790ff6e3d9
mikolaszko <mikosodz.lote@gmail.com>
2023-06-03 13:17:55 +0200

    Router with wrong route handling and handy built-in functions

--- first-php-vanilla-website/index.php
+++ first-php-vanilla-website/index.php
@@ -1,12 +1,4 @@
 <?php
 
 require 'utils/functions.php';
-$uri = $_SERVER['REQUEST_URI'];
-
-if ($uri === '/') {
-    require 'controllers/index.php';
-} elseif ($uri === '/about') {
-    require 'controllers/about.php';
-} elseif ($uri === '/contact') {
-    require 'controllers/contact.php';
-} 
+require 'router.php';
